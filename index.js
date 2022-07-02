1aa3e851f0d46f9c3a95c94124e33f5b152b1ed2
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-07-02 12:34:10 +0530

    add content to file

---
+++ index.js
@@ -0,0 +1,28 @@
+let movies = [
+  {
+    name: "falcon and the winter soldier",
+    des: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta animi commodi illum, voluptates totam corporis",
+    image: "images/slider 2.png"
+  },
+  {
+    name: "loki",
+    des: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta animi commodi illum, voluptates totam corporis",
+    image: "images/slider 1.png"
+  },
+  {
+    name: "wanda vision",
+    des: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta animi commodi illum, voluptates totam corporis",
+    image: "images/slider 3.png"
+  },
+  {
+    name: "raya and the last dragon",
+    des: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta animi commodi illum, voluptates totam corporis",
+    image: "images/slider 4.png"
+  },
+  {
+    name: "luca",
+    des: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta animi commodi illum, voluptates totam corporis",
+    image: "images/slider 5.png"
+  },
+
+]
