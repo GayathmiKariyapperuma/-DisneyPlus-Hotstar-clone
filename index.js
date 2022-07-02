07ccf13dda2a3c4fdf16236e637e5a9c41558d9a
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-07-02 14:47:56 +0530

    bug fix

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ let movies = [
   {
     name: "falcon and the winter soldier",
     des:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta animi commodi illum, voluptates totam corporis",
-    image: "images/slider 2.png"
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "images/slider 2.PNG"
   },
   {
     name: "loki",
     des:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta animi commodi illum, voluptates totam corporis",
-    image: "images/slider 1.png"
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "images/slider 1.PNG"
   },
   {
     name: "wanda vision",
     des:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta animi commodi illum, voluptates totam corporis",
-    image: "images/slider 3.png"
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "images/slider 3.PNG"
   },
   {
     name: "raya and the last dragon",
     des:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta animi commodi illum, voluptates totam corporis",
-    image: "images/slider 4.png"
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "images/slider 4.PNG"
   },
   {
     name: "luca",
     des:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta animi commodi illum, voluptates totam corporis",
-    image: "images/slider 5.png"
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "images/slider 5.PNG"
   }
 ];
 
@@ -69,9 +69,10 @@ const createSlide = () => {
 
   sliders.push(slide);
 
-  if (slide.length) {
-    sliders[0].style.marginLeft =
-      "calc(-${100 * (sliders.length - 2)}% - ${30 * (sliders.length - 2)}px)";
+  if (sliders.length) {
+    sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
+      30 * (sliders.length - 2)
+    }px)`;
   }
 };
 
